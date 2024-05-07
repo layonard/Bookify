bbd80b7fa85f7d1aac863067653b614d29603eb3
Leonardo Vargas <leonardo.vargas@niceincontact.com>
2024-05-07 01:25:14 -0400

    Create Appartment entity

---
+++ Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Money (decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money left, Money right)
+    {
+        if (left.Currency != right.Currency)
+        {
+            throw new InvalidOperationException("Currencies have to be equal");
+        }
+
+        return new Money(left.Amount + right.Amount, left.Currency);
+    }
+
+    public static Money Zero() => new Money(0, Currency.None);
+}
